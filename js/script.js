b10e9df07b3c0bc86837c8e9d7e0a22709550de9
sctlcd <lucinda.souchet@outlook.com>
2020-04-09 00:06:39 +0000

    update style.css, index.html and script.js

--- js/script.js
+++ js/script.js
@@ -32,3 +32,13 @@ $(document).ready(function() {
     	}
     });
 })
+
+// when clicking on the <li> elements Module1, Module 2 or Module 3 and the background of the cards associated with 
+//them will change to having a red background.
+$(".module-nav").on("click", function() {
+  let allModulesCardsSelector = ".card"
+  let thisModuleCardsSelector = "." + this.id + "-card"
+  
+  $(allModulesCardsSelector).removeClass("card-highlight");
+  $(thisModuleCardsSelector).addClass("card-highlight");
+});
